1db82c35c6bf41f7ebcd8f52f3e316e0d934b3a1
harshpalkar <explorewitharsh@outlook.com>
2022-09-27 12:22:52 +0530

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { AppComponent } from './app.component';
 import { LibraryComponent } from './library/library.component';
 import { UserLoginComponent } from './library/user-login/user-login.component';
 import { LandingPageComponent } from './landingPage/landingPage.component';
-import { HeaderComponent } from './landingPage/header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { booksRoutingModule } from './books/books-routing-module';
 import { appRoutingModule } from './app-routing-module';
 import { landingpageRoutingModule } from './landingPage/landingpage-routing-module';
 import { SharedModule } from './shared/shared.module';
 import { coreModule } from './core/core.module';
-import { booksModule } from './books/books.module';
-import { AuthGuard, canDeactivateGuard } from './authGuard.service';
-import { AuthServiceService } from './authService.service';
 
 
 @NgModule({
