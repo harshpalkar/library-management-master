ae1b0bc8a9f0ac5a09f85e4e128a72247748ee09
harshpalkar <explorewitharsh@outlook.com>
2022-10-13 14:56:45 +0530

    master

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './app/landingPage/environments/environment';
+// import { environment } from './app/landingPage/environments/environment';
+import { environment } from './environments/environment.prod';
 
 if (environment.production) {
   enableProdMode();
