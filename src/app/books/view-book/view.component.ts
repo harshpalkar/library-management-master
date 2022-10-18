1e2067850cddd54aa83d6990326f75966ab75952
harshpalkar <explorewitharsh@outlook.com>
2022-10-18 17:23:26 +0530

    branch

--- src/app/books/view-book/view.component.ts
+++ src/app/books/view-book/view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BooksService } from 'src/app/core/appService/books.service';
 
 @Component({
@@ -10,7 +10,7 @@ import { BooksService } from 'src/app/core/appService/books.service';
 
 export class ViewComponent implements OnInit {
   
-  constructor(private route : ActivatedRoute, private _books : BooksService) {
+  constructor(private route : ActivatedRoute, private _router : Router, private _books : BooksService) {
     this.hi = this._books.data
   }
   
@@ -32,4 +32,8 @@ export class ViewComponent implements OnInit {
   id:string = 'id';
   author:string = 'author'
   books:string = 'book'
+
+  backBtn(){
+    this._router.navigate(['./books'])
+  }
 }
